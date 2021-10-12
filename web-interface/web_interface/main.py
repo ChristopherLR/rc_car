a69f657e62b994f6a88954f6475137b7ed961042
Christopher Michael Sutton <christopher.sutton12@gmail.com>
2021-10-12 15:27:21 +1100

    reverted to simple streaming response

--- web-interface/web_interface/main.py
+++ web-interface/web_interface/main.py
@@ -6,28 +6,6 @@ from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
 from imutils.video import VideoStream
 import cv2
-<<<<<<< HEAD
-from websockets import serve
-import asyncio
-from threading import Thread
-
-
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-CLIENTS = set()
-
-@app.get('/')
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "test": 'key'})
-
-global camera
-camera = cv2.VideoCapture(0)
-
-async def video_stream():
-    print("Running Video Stream")
-    while True:
-        ret, frame = camera.read()
-=======
 import asyncio
 from threading import Thread, Lock
 
@@ -44,66 +22,22 @@ async def root(request: Request):
 
 def gen():
     global frame
->>>>>>> fdf3e9f (reverted to simple streaming response)
 
     while True:
 
         frame = vs.read()
         r, jpg = cv2.imencode('.jpg', frame)
-<<<<<<< HEAD
-        bytes_jpg = jpg.tobytes()
-        
-        for ws in CLIENTS:
-            await ws.send(bytes_jpg)
-
-async def handler(websocket, path):
-    CLIENTS.add(websocket)
-    try:
-        async for msg in websocket:
-            pass
-    finally:
-        CLIENTS.remove(websocket)
-
-def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
-    asyncio.set_event_loop(loop)
-    loop.run_forever()
-=======
 
         if not r:
             continue
 
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + jpg.tobytes() + b'\r\n')
                
->>>>>>> fdf3e9f (reverted to simple streaming response)
 
 def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
     asyncio.set_event_loop(loop)
     loop.run_forever()
 
-<<<<<<< HEAD
-async def socket_server() -> None:
-    async with serve(handler, '0.0.0.0', 8765):
-        await asyncio.Future()
-
-
-def start():
-    loop1 = asyncio.new_event_loop()
-    loop2 = asyncio.new_event_loop()
-    t1 = Thread(target=start_background_loop, args=(loop1,), daemon=True)
-    t2 = Thread(target=start_background_loop, args=(loop2,), daemon=True)
-    t1.start()
-    t2.start()
-
-    asyncio.run_coroutine_threadsafe(socket_server(), loop1)
-    asyncio.run_coroutine_threadsafe(video_stream(), loop2)
-
-    uvicorn.run("web_interface.main:app", host="0.0.0.0", port=8080, reload=True),
-    loop1.stop()
-    loop2.stop()
-
-if __name__ == '__main__':
-    start()
-=======
 @app.get('/video') # Another endpoint for the index to call to generate the image feed
 def video_feed():
     return StreamingResponse(gen(), media_type="multipart/x-mixed-replace;boundary=frame")
@@ -111,4 +45,3 @@ def video_feed():
 def start():
     """Run app using `poetry run start`"""
     uvicorn.run("web_interface.main:app", host="127.0.0.1", port=8080, reload=True)
->>>>>>> fdf3e9f (reverted to simple streaming response)
