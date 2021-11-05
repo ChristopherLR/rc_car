b8d801a9ac58adac186a8d6c780c0905b3f3c5a6
chris <christopher.sutton12@gmail.com>
2021-11-05 11:55:56 +1100

    added color detection and following

--- image_gen/image_gen/main.py
+++ image_gen/image_gen/main.py
@@ -7,10 +7,15 @@ import async_timeout
 import aioamqp
 import signal
 import functools
+import numpy as np
+import json
+import time
 
 vs = VideoStream(src=0).start()
 redis = aioredis.from_url("redis://localhost")
 pubsub = redis.pubsub()
+global target
+target = None
 
 async def callback(ch, body, envelope, properties):
     print(f"recv: {body}")
@@ -22,10 +27,53 @@ async def event_stream():
     channel = await protocol.channel()
     await channel.basic_consume(callback, queue_name="state", no_ack=True)
 
+async def target_stream():
+    global target
+    transport, protocol = await aioamqp.connect()
+    channel = await protocol.channel()
+    while True:
+        if target != None:
+            await channel.basic_publish(
+                    payload=json.dumps(target),
+                    exchange_name='amq.topic',
+                    routing_key='target.position'
+            )
+            x, y = target
+            await redis.publish('channel:1', f'Target Position {x}, {y}')
+            print(f'pub {target}')
+        else:
+            await channel.basic_publish(
+                    payload=json.dumps((-1, -1)),
+                    exchange_name='amq.topic',
+                    routing_key='target.position'
+            )
+
+        await asyncio.sleep(0.5)
+
 async def video_stream():
+    lowerBound=np.array([33,80,40])
+    upperBound=np.array([102,255,255])
     print('starting video stream')
     while True:
         frame = vs.read()
+        frame=cv2.resize(frame,(340,220))
+        imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(imgHSV, lowerBound, upperBound)
+        kernelOpen = np.ones((5,5))
+        kernelClose = np.ones((20,20))
+        maskOpen = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+        maskClose = cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+        maskFinal=maskClose
+        conts,h=cv2.findContours(maskFinal,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+        if len(conts) > 0:
+            M = cv2.moments(conts[0])
+            global target
+            target = (round(M['m10'] / M['m00']), round(M['m01'] / M['m00']))
+            cv2.circle(frame, target, 5, (0, 255, 0), -1)
+        else:
+            target = None
+        cv2.drawContours(frame,conts,-1,(255,0,0),3)
+
         r, jpg = cv2.imencode('.jpg', frame)
         await redis.set('video', jpg.tobytes())
 
@@ -43,6 +91,7 @@ def start():
         loop.add_signal_handler(getattr(signal, signame), functools.partial(stopper, signame, loop))
     loop.create_task(video_stream())
     loop.create_task(event_stream())
+    loop.create_task(target_stream())
     loop.run_forever()
 
 if __name__ == '__main__':
--- state_machine/state_machine/main.py
+++ state_machine/state_machine/main.py
@@ -6,6 +6,8 @@ import aioamqp
 import asyncio
 import functools
 import signal
+import json
+import time
 
 class Direction(Enum):
     forward = 'forward'
@@ -18,8 +20,9 @@ class Direction(Enum):
 class MotorState:
     direction: Direction
     throttle: float
+    autonomous: bool = False
 
-MOTOR_STATE = MotorState(Direction.stop, 0)
+MOTOR_STATE = MotorState(Direction.stop, 0, False)
 
 async def setup_queues():
     transport, protocol = await aioamqp.connect()
@@ -28,14 +31,43 @@ async def setup_queues():
     await channel.queue_bind(exchange_name="amq.topic", queue_name="motor_state", routing_key="motor.state")
     await channel.queue_declare(queue_name='state', durable=True)
     await channel.queue_bind(exchange_name="amq.topic", queue_name="state", routing_key="*.state")
+    await channel.queue_declare(queue_name='target', durable=True)
+    await channel.queue_bind(exchange_name="amq.topic", queue_name="target", routing_key="target.*")
 
 async def ui_cb(ch, body, envelope, properties):
     print("Received %r" % body)
-    await ch.basic_publish(
-        payload=body,
-        exchange_name='amq.topic',
-        routing_key='motor.state'
-    )
+    msg = json.loads(body)
+    if msg['direction'] == 'auto_on': MOTOR_STATE.autonomous = True
+    if msg['direction'] == 'auto_off': MOTOR_STATE.autonomous = False
+    if MOTOR_STATE.autonomous == False or msg['direction'] == "stop":
+        await ch.basic_publish(
+            payload=body,
+            exchange_name='amq.topic',
+            routing_key='motor.state'
+        )
+
+async def target_cb(ch, body, envelope, properties):
+    print("Received %r" % body)
+    target = json.loads(body)
+    [x, y] = target
+    if MOTOR_STATE.autonomous:
+        msg = None
+        if x < 0:
+            msg = {'direction': 'stop', 'throttle': 0, 'ts': int(time.time()) }
+        elif x < 100:
+            print('l')
+            msg = {'direction': 'left', 'throttle': 50, 'ts': int(time.time()) }
+        elif x > 200:
+            print('r')
+            msg = {'direction': 'right', 'throttle': 50, 'ts': int(time.time()) }
+        else:
+            print('s')
+            msg = {'direction': 'forward', 'throttle': 30, 'ts': int(time.time()) }
+        await ch.basic_publish(
+            payload=json.dumps(msg),
+            exchange_name='amq.topic',
+            routing_key='motor.state'
+        )
 
 async def ui_stream():
     transport, protocol = await aioamqp.connect()
@@ -43,6 +75,12 @@ async def ui_stream():
     await channel.queue_declare(queue_name='ui_commands')
     print('UI Stream Started')
     await channel.basic_consume(ui_cb, queue_name="ui_commands", no_ack=True)
+
+async def target_stream():
+    transport, protocol = await aioamqp.connect()
+    channel = await protocol.channel()
+    print('Target Stream Started')
+    await channel.basic_consume(target_cb, queue_name="target", no_ack=True)
 
 async def state_publish():
     transport, protocol = await aioamqp.connect()
@@ -62,6 +100,7 @@ def start():
 
     loop.create_task(setup_queues())
     loop.create_task(ui_stream())
+    loop.create_task(target_stream())
     loop.run_forever()
 
 if __name__ == '__main__':
